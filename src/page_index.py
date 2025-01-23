611563c0180eeccd7b940516dfd873b95f5608c6
fannina22 <aaronfannin10@gmail.com>
2025-01-23 14:47:53 -0500

    made things better

--- src/page_index.py
+++ src/page_index.py
@@ -1,16 +1,9 @@
 import mako.template
 import os.path
 
-#demo
-import datetime
-
-#location of this file
 srcdir = os.path.dirname(__file__)
 
 def get():
-    today=datetime.datetime.now()
     T = mako.template.Template(filename=f"{srcdir}/../html/home.html")
-    return T.render(
-        today=f"{today.month}/{today.day}/{today.year}"
-    )
+    return T.render()
     
